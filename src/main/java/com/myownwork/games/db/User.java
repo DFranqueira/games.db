bf4f81a3fb912c1ca162c3400a9e497ef245a9f1
DFranqueira <diogo.franqueira@gmail.com>
2020-07-31 11:55:49 +0100

    Update da class User

--- src/main/java/com/myownwork/games/db/User.java
+++ src/main/java/com/myownwork/games/db/User.java
@@ -9,12 +9,17 @@ import javax.persistence.Id;
 public class User {
   @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
+ 
   private Integer id;
-
   private String name;
-
   private String email;
-
+  private String username;
+  private String password;
+  private String status;
+  
+  
+  
+  //Getter and Setters
   public Integer getId() {
     return id;
   }
@@ -38,4 +43,29 @@ public class User {
   public void setEmail(String email) {
     this.email = email;
   }
+  
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+  
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+  
 }
