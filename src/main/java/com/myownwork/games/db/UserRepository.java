cf9de6601eea8394bd215095cb519250fb0cafac
DFranqueira <diogo.franqueira@gmail.com>
2020-07-31 11:19:36 +0100

    First commit. Working Project

---
+++ src/main/java/com/myownwork/games/db/UserRepository.java
@@ -0,0 +1,12 @@
+package com.myownwork.games.db;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.myownwork.games.db.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
